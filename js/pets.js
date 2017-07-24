95b0f75354d8f206fce7498be4552879e1c1b529
Christine Wang <wang.christiney@gmail.com>
2017-07-24 11:13:21 -0700

    created js folder and pets.js

---
+++ js/pets.js
@@ -0,0 +1,14 @@
+//Pet Options
+
+//mouse  <:3 )~~~~
+//cat    (=^o.o^=)__
+//fish   <`)))><
+
+
+function feed(pet) {
+  if (pet.hungry === true) {
+     //TODO
+  } else  {
+    console.log("I'm full. I already ate!");
+  }
+}
